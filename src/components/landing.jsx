d59cf0539e383a5c802965b96a046758ff1036b2
Takvor Garabedian <takvor.garabedian@datin.io>
2024-07-15 22:40:41 +0300

    Base routing

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,12 +1,13 @@
 import projectLogo from "/images/seat.png";
 import FileUpload from "./fileupload.jsx";
+import { Routes, Route, Link } from "react-router-dom";
 
 export default function Landing() {
     return (
       <>
           <div>
-              <a href="#" target="_blank">
-                  <img src={projectLogo} className=" logo picture-background" alt="Car logo"/>
+              <a href="/#" target="_blank">
+                  <img src={projectLogo} className="logo picture-background" alt="Car logo"/>
               </a>
           </div>
           <h1 className="emboss">Auto Loook</h1>
