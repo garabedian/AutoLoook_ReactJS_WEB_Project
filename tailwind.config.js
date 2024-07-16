4b17804f055ba8a4fadcb27146c3d1d4e41b3fa7
Takvor Garabedian <takvor.garabedian@datin.io>
2024-07-16 22:31:51 +0300

    Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  prefix: 'tw-', // prefix for all css classes
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
