1a643b514a7e2565af080e2a867b2a9dc0de2910
Takvor Garabedian <takvor.garabedian@datin.io>
2024-07-18 22:44:05 +0300

    Improvement

--- src/components/maincontent.jsx
+++ src/components/maincontent.jsx
@@ -6,8 +6,10 @@ export default function MainContent({ isAuthenticated }) {
     return (
       <>
           <div>
-              <button style={{ backgroundColor: "transparent", border: "none" }} onClick={() => window.location.reload()}>
-                  <img src={projectLogo} className="logo picture-background animate__animated animate__bounceInDown" alt="Car logo"/>
+              <button style={{ backgroundColor: "transparent", border: "none" }}
+                      onClick={() => window.location.reload()}>
+                  <img src={projectLogo} className="logo picture-background animate__animated animate__bounceInDown"
+                       alt="Car logo"/>
               </button>
           </div>
           <br/>
@@ -16,42 +18,45 @@ export default function MainContent({ isAuthenticated }) {
            text-center animate__animated animate__bounceInRight"><strong>Welcome
               to</strong></h1>
           <h1 className="emboss text-center animate__animated animate__bounceInLeft">Auto Loook</h1>
-          <p className="lead text-center animate__animated animate__bounceInRight" style={{color: "pink"}}><em><strong>Share the cars
-              and vehicles you own or like.</strong></em></p>
-          {!isAuthenticated() && (
-            <>
-                <hr className="my-4"/>
-                <div className="inner cover animate__animated animate__zoomInUp">
-                    <Row>
-                        <Col lg={6}>
-                            <p className="lead" style={{color: "pink"}}>Registered user? <br/><Link
-                              to="/AutoLoook_ReactJS_WEB_Project/login" className="btn btn-lg btn-secondary">sign
-                                in</Link></p>
-                        </Col>
-                        <Col lg={6}>
-                            <p className="lead" style={{color: "pink"}}>Welcome, guest!<br/><Link
-                              to="/AutoLoook_ReactJS_WEB_Project/register" className="btn btn-lg btn-secondary">sign
-                                up</Link></p>
-                        </Col>
-                    </Row>
-                </div>
-            </>
-          )}
-          <hr className="my-4"/>
-          <div className="inner cover animate__animated animate__zoomIn">
-              <Row>
-                  <Col lg={6}>
-                      <p className="lead" style={{color: "pink"}}>Free public zone&ensp;<br/><Link
-                        to="/AutoLoook_ReactJS_WEB_Project/list-items" className="btn btn-lg btn-secondary">List
-                          all vehicles</Link></p>
-                  </Col>
-                  <Col lg={6}>
-                      <p className="lead" style={{color: "pink"}}>Logged in users can<br/><Link
-                        to="/AutoLoook_ReactJS_WEB_Project/create-item" className="btn btn-lg btn-secondary">Create
-                          a vehicle</Link></p>
-                  </Col>
-              </Row>
+          <p className="lead text-center animate__animated animate__bounceInRight" style={{ color: "pink" }}>
+              <em><strong>Share the cars
+                  and vehicles you own or like.</strong></em></p>
+          <div className="logo picture-background" >
+              {!isAuthenticated() && (
+                <>
+                    <div className="inner cover animate__animated animate__zoomInUp">
+                        <Row>
+                            <Col lg={6}>
+                                <p className="lead" style={{ color: "pink" }}>Registered user? <br/><Link
+                                  to="/AutoLoook_ReactJS_WEB_Project/login" className="btn btn-lg btn-secondary">sign
+                                    in</Link></p>
+                            </Col>
+                            <Col lg={6}>
+                                <p className="lead" style={{ color: "pink" }}>Welcome, guest!<br/><Link
+                                  to="/AutoLoook_ReactJS_WEB_Project/register" className="btn btn-lg btn-secondary">sign
+                                    up</Link></p>
+                            </Col>
+                        </Row>
+                    </div>
+                </>
+              )}
+              <hr className="my-2"/>
+              <div className="inner cover animate__animated animate__zoomIn">
+                  <Row>
+                      <Col lg={6}>
+                          <p className="lead" style={{ color: "pink" }}>Free public zone&ensp;<br/><Link
+                            to="/AutoLoook_ReactJS_WEB_Project/list-items" className="btn btn-lg btn-secondary">List
+                              all vehicles</Link></p>
+                      </Col>
+                      <Col lg={6}>
+                          <p className="lead" style={{ color: "pink" }}>Logged in users can<br/><Link
+                            to="/AutoLoook_ReactJS_WEB_Project/create-item" className="btn btn-lg btn-secondary">Create
+                              a vehicle</Link></p>
+                      </Col>
+                  </Row>
+              </div>
           </div>
+          <br/>
       </>
     );
 };
