a1e427b5015f6378973e061c6bf2c3a517991abd
Takvor Garabedian <takvor.garabedian@datin.io>
2024-08-03 21:05:22 +0300

    Created protected-route.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import AddItem from './components/add-item.jsx';
 import ListCars from "./components/list-items.jsx";
 import NotYetImplemented from './components/not-yet-implemented.jsx';
 import CarDetails from "./components/item-details.jsx";
+import ProtectedRoute from './components/protected-route.jsx';
 
 function App() {
     const [showImage, setShowImage] = useState(true);
@@ -46,8 +47,8 @@ function App() {
               <Route path="/AutoLoook_ReactJS_WEB_Project/register" element={<Register/>}/>
               <Route path="/AutoLoook_ReactJS_WEB_Project/file-upload" element={<FileUpload/>}/>
               <Route path="/AutoLoook_ReactJS_WEB_Project/re-login" element={<Relogin/>}/>
-              <Route path="/AutoLoook_ReactJS_WEB_Project/list-items" element={<ListCars />} />
-              <Route path="/AutoLoook_ReactJS_WEB_Project/car/:id" element={<CarDetails />} />
+              <Route path="/AutoLoook_ReactJS_WEB_Project/car/:id" element={<ProtectedRoute element={CarDetails} />} />
+              <Route path="/AutoLoook_ReactJS_WEB_Project/create-item" element={<ProtectedRoute element={AddItem} />} />
               <Route path="/AutoLoook_ReactJS_WEB_Project/create-item"
                      element={user ? <AddItem/> : <Navigate to="/AutoLoook_ReactJS_WEB_Project/login"/>}/>
               <Route path="/AutoLoook_ReactJS_WEB_Project/not-yet-implemented" element={<NotYetImplemented />} />
